fda711e9a3bc128da95888dc97cca609462aa82f
ivanxoxol <for.ivans.work@gmail.com>
2022-04-27 21:39:18 +0300

    make some changes, add export to csv, return to holistic mode

--- video_processing.py
+++ video_processing.py
@@ -1,70 +1,82 @@
 import cv2
-import csv
-import os
 import numpy as np
 import mediapipe as mp
 import pathlib
 from pathlib import Path
+import csv
+import os
 
 mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+mp_holistic = mp.solutions.holistic
 
+# Get video path
 video_path = Path(pathlib.Path.cwd(), "video", "acrobatics.mp4")
 cap = cv2.VideoCapture(str(video_path))
 
-# fps = float(cap.get(cv2.CAP_PROP_FPS))
-# w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# Make csv file profile
+num_coords = 33
+landmarks = []
+for val in range(1, num_coords+1):
+    landmarks += [  'x{}'.format(val), 
+                    'y{}'.format(val), 
+                    'z{}'.format(val), 
+                    'v{}'.format(val)   ]
+with open('coords.csv', mode='w', newline='') as f:
+    csv_writer = csv.writer(f, delimiter=',', 
+                            quotechar='"', 
+                            quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(landmarks)
 
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
+# Make detections using mediapipe holistic mode
+with mp_holistic.Holistic(
+                    min_detection_confidence=0.5,
+                    min_tracking_confidence=0.5     ) as holistic:
+    
     while cap.isOpened():
+
+        # Check the video stream
         ret, frame = cap.read()
         if not ret:
             print("Ignoring empty camera frame.")
             break
-            
-        frame.flags.writeable = False
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = pose.process(image)
         
-        if results.pose_landmarks:
-            print(
-                f'Nose coordinates: ('
-                f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x}, '
-                f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y})'
-            )
-        # face_landmarks, pose_landmarks, 
-        # left_hand_landmarks, right_hand_landmarks
+        # Get results
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        results = holistic.process(image)
 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        """ # 1. Draw face landmarks
-        mp_drawing.draw_landmarks(image, results.face_landmarks, 
-                                mp_holistic.FACE_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(20,143,209), thickness=2, circle_radius=1),
-                                mp_drawing.DrawingSpec(color=(7,95,143), thickness=2, circle_radius=2)
-                                )
-        # 2. Right hand
-        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, 
-                                mp_holistic.HAND_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(70,177,137), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(38,110,84), thickness=2, circle_radius=2)
-                                )
-        # 3. Left Hand
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, 
-                                mp_holistic.HAND_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(70,177,137), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(38,110,84), thickness=2, circle_radius=2)
-                                ) """
-        # 4. Pose Detections
+        
+        # Pose Detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, 
-                                mp_pose.POSE_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(166,83,147), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(92,28,78), thickness=2, circle_radius=2)
-                                )
-
+                                mp_holistic.POSE_CONNECTIONS, 
+                                mp_drawing.DrawingSpec(color=(166,83,147), 
+                                                        thickness=2, 
+                                                        circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(92,28,78), 
+                                                        thickness=2, 
+                                                        circle_radius=2))
+        
+        # Export coordinates
+        try:
+            pose = results.pose_landmarks.landmark
+            pose_row = list(np.array([[landmark.x, 
+                                        landmark.y, 
+                                        landmark.z, 
+                                        landmark.visibility] 
+                                    for landmark in pose]).flatten())
+            
+            with open('coords.csv', mode='a', newline='') as f:
+                csv_writer = csv.writer(f, delimiter=',', 
+                                        quotechar='"', 
+                                        quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(pose_row) 
+        except:
+            pass
+        
+        # Show video processing in window
         cv2.imshow('video processing', image)
 
         if cv2.waitKey(5) & 0xFF == ord('q'):
